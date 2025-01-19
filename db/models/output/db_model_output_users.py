56cdb5e91df1593e51c4f442c966707990abad82
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-01-19 19:21:11 +0300

    скорректирована выходная модель БД

--- db/models/output/db_model_output_users.py
+++ db/models/output/db_model_output_users.py
@@ -1,20 +1,10 @@
-import datetime
-
 from pyqaunicore.validators.validator_pydantic import ValidatorPydantic
 
-# from pyqaunicore.templates.prj_minimal.db.db_provider import DBProvider
 from db.models.input.db_model_input_users import DBModelInputUser
 
 
 class DBModelOutputUser(DBModelInputUser, ValidatorPydantic.BaseModel):
-    # __table_args__: Optional[dict] = {'schema': 'dbo'}
-    # __tablename__: str | None = 'users'
-    id: int | None = None
-    name: str
-    age: int
-    created_at: int = int(datetime.datetime.now().timestamp())
-    is_deleted: bool = False
-    is_blocked: bool = False
+    pass
 
 
 class DBModelOutputUsers(ValidatorPydantic.RootModel):
