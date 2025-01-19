9dcbe94aedc3b84863db5f649a6180e1d6c2c43a
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-01-19 14:29:11 +0300

    доработан импорт логгера

--- support/loggers/testrun_logger.py
+++ support/loggers/testrun_logger.py
@@ -1,13 +1,10 @@
 """Модуль логгера"""
 
 # Раскомментировать для логгера LoggerLoguru
-from pyqaunicore.loggers.logger_loguru import LoggerLoguru
-
-from config.config_general import config_general
-
-logger = LoggerLoguru(config_general.log_file_path)
+from pyqaunicore.loggers.logger_loguru import LoggerLoguru as TestrunLogger
 
 # Раскомментировать для логгера LoggerLogging
-# from pyqaunicore.loggers.logger_logging import LoggerLogging
+# from pyqaunicore.loggers.logger_logging import LoggerLogging as TestrunLogger
+from config.config_general import config_general
 
-# logger = LoggerLogging(config_general.log_file_path)
+logger = TestrunLogger(config_general.log_file_path)
