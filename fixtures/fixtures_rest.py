bfb4bcb30a65e936fae9327056e3cd1d8136c015
Dmitry Vorobjev <dmitry.vorobjev@systeme.ru>
2025-04-15 10:36:59 +0300

    обновлена версия ядра

--- fixtures/fixtures_rest.py
+++ fixtures/fixtures_rest.py
@@ -1,6 +1,6 @@
 import pytest
 from pyqaunicore.api import HTTPClient, HTTPReqSenderBase
-from pyqaunicore.api import HTTPReqSenderRequests as HTTPReqSender
+from pyqaunicore.api.http.http_req_sender_requests import HTTPReqSenderRequests as HTTPReqSender
 
 # from pyqaunicore.api import HTTPReqSenderAiohttp as HTTPReqSender
 # from pyqaunicore.api import HTTPReqSenderHttpx as HTTPReqSender
